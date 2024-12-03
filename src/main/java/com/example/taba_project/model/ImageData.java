7370ecf0c320e05d85d8f9be72544458e786f01b
장재훈 <jhjjang0423@naver.com>
2024-12-03 17:48:06 +0900

    DB 의존성 고려하지 않은 Version(applicaion.properties)에 코드 추가

--- src/main/java/com/example/taba_project/model/ImageData.java
+++ src/main/java/com/example/taba_project/model/ImageData.java
@@ -1,20 +1,3 @@
-//package com.example.demo.model;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@Data
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class ImageData {
-//    private int width;               // 이미지 너비
-//    private int height;              // 이미지 높이
-//    private String format;           // 이미지 포맷
-//    private int planes;              // 평면 수 (YUV 포맷)
-//    private byte[] imageData;        // 바이너리 이미지 데이터
-//}
-
 package com.example.taba_project.model;
 
 import lombok.AllArgsConstructor;
