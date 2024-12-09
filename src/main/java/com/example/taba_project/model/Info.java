a9b775b1ef391cb19b61a8ed6cca93a0409fd96d
Dev.gunnuuu <105582355+gunnuuu@users.noreply.github.com>
2024-12-10 01:44:50 +0900

    Update Info.java

--- src/main/java/com/example/taba_project/model/Info.java
+++ src/main/java/com/example/taba_project/model/Info.java
@@ -1,11 +1,11 @@
 package com.example.taba_project.model;
 
-import lombok.Data;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.Getter;
 
 @Data
 @Entity
@@ -15,23 +15,11 @@ public class Info {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String age;
-
-    private String gender;
+    private Integer age;
 
     private String emotion;
 
-    private String percentage;
+    private String gender;
 
-    // Getter and Setter methods (Lombok @Data가 자동으로 생성해 줌)
-
-    // 만약 JSON에서 "idInfo"라는 이름을 계속 쓰고 싶다면, 아래와 같이 @JsonProperty 사용
-    @JsonProperty("idInfo")
-    public Long getId() {
-        return id;
-    }
-
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
+    private Double percentage;
 }
