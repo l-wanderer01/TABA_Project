64c2aea83bb69a86e6e3ca54b2bd4873d1b83f1a
Dev.gunnuuu <105582355+gunnuuu@users.noreply.github.com>
2024-12-10 01:44:38 +0900

    Update Image.java

--- src/main/java/com/example/taba_project/model/Image.java
+++ src/main/java/com/example/taba_project/model/Image.java
@@ -1,11 +1,10 @@
 package com.example.taba_project.model;
 
-import lombok.Data;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
 @Data
