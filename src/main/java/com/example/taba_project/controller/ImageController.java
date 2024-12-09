07d2ee9d6c233865d67a2af630312e34c23051df
Dev.gunnuuu <105582355+gunnuuu@users.noreply.github.com>
2024-12-10 01:43:39 +0900

    Update ImageController.java

--- src/main/java/com/example/taba_project/controller/ImageController.java
+++ src/main/java/com/example/taba_project/controller/ImageController.java
@@ -2,12 +2,9 @@ package com.example.taba_project.controller;
 
 import com.example.taba_project.handler.FileStorageHandler;
 import com.example.taba_project.model.Image;
+import com.example.taba_project.model.Info;
 import com.example.taba_project.repository.ImageRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -16,36 +13,19 @@ import java.io.IOException;
 @RequestMapping("/api/image")
 public class ImageController {
 
-    private final ImageRepository imageRepository;
-    private final FileStorageHandler fileStorageHandler;
-    private static final Logger logger = LoggerFactory.getLogger(ImageController.class);
+    @Autowired
+    private ImageRepository imageRepository;
 
-    @Value("${file.storage.directory")
-    private String directoryPath;
-
-    public ImageController(ImageRepository imageRepository, FileStorageHandler fileStorageHandler) {
-        this.imageRepository = imageRepository;
-        this.fileStorageHandler = fileStorageHandler;
-    }
-
-    @PostMapping("/upload")
-    public ResponseEntity<String> uploadImage(@RequestParam("fileName") String fileName,
-                                              @RequestBody byte[] fileData) {
+    @PostMapping
+    public String saveUrl(@RequestBody Image image) {
         try {
-            // 파일 저장
-            String savedFilePath = fileStorageHandler.saveFile(directoryPath, fileName, fileData);
-
             // DB에 저장
-            Image image = new Image();
-            image.setUrl(savedFilePath); // 파일 경로를 url로 설정
             imageRepository.save(image);
 
-            return ResponseEntity.ok("파일 업로드 및 DB 저장 성공: " + savedFilePath);
-
-        } catch (IOException e) {
-            logger.error("파일 저장 실패", e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("파일 저장 실패: " + e.getMessage());
+            // 결과 반환
+            return image.getUrl();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 }
