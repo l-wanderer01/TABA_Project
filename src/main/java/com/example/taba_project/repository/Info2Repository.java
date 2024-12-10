5f46b7f29de67567f7fbeb284ec5530cf5339749
Dev.gunnuuu <105582355+gunnuuu@users.noreply.github.com>
2024-12-10 14:01:37 +0900

    Create Info2Repository.java

---
+++ src/main/java/com/example/taba_project/repository/Info2Repository.java
@@ -0,0 +1,8 @@
+package com.example.taba_project.repository;
+
+import com.example.taba_project.model.Info2;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Info2Repository extends JpaRepository<Info2, Long> {
+
+}
