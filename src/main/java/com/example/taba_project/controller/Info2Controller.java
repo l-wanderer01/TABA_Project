2c0bb32e90991410d2ba8a036da226e5751d1350
Dev.gunnuuu <105582355+gunnuuu@users.noreply.github.com>
2024-12-10 14:27:45 +0900

    Update Info2Controller.java

--- src/main/java/com/example/taba_project/controller/Info2Controller.java
+++ src/main/java/com/example/taba_project/controller/Info2Controller.java
@@ -1,5 +1,6 @@
 package com.example.taba_project.controller;
 
+import com.example.taba_project.model.Info;
 import com.example.taba_project.model.Info2;
 import com.example.taba_project.repository.Info2Repository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/info2")
+@RequestMapping("/api/secondinfo")
 public class Info2Controller {
 
     @Autowired
@@ -17,16 +18,35 @@ public class Info2Controller {
 
     // 새로운 Info 저장
     @PostMapping
-    public void saveInfo(@RequestBody Info2 info2) {
+    public String saveInfo(@RequestBody Info2 info2) {
         try {
             System.out.println("Received info: " + info2);
 
             // DB에 저장
             info2Repository.save(info2);
 
+            // 경고 메시지 생성
+            String warningMessage = generateWarningMessage(info2.getLocation());
+
+            // 로그에 출력
+            System.out.println("Generated warning Message: " + warningMessage);
+
+            // 결과 반환
+            return warningMessage;
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
             e.printStackTrace();
+            return "Error occurred";
         }
+    }
+
+    private String generateWarningMessage(String location) {
+        String message = "";
+
+        String[] points  = location.split(",");
+        //여기서 좌표들 가지고 거리 대비 메세지 스크립트 작성하기 , 테스트 우선 필요할듯
+
+
+        return message;
     }
 }
