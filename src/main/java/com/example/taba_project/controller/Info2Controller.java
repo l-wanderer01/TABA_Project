3a120977f9ee2f5982575b50d2288fe28f37af34
장재훈 <jhjjang0423@naver.com>
2024-12-15 22:08:06 +0900

    Info2Controller.java의 RequestMapping url을 api/info2/move로 수정

--- src/main/java/com/example/taba_project/controller/Info2Controller.java
+++ src/main/java/com/example/taba_project/controller/Info2Controller.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/info")
+@RequestMapping("/api/info2")
 public class Info2Controller {
 
     @Autowired
