190a9e995ed80d5f197cbb0a434c81b32af75877
Dev.gunnuuu <105582355+gunnuuu@users.noreply.github.com>
2024-12-17 13:43:02 +0900

    Update WebSocketConfig.java

--- src/test/java/com/example/taba_project/config/WebSocketConfig.java
+++ src/test/java/com/example/taba_project/config/WebSocketConfig.java
@@ -1,25 +1,22 @@
 package com.example.taba_project.config;
 
+import com.example.taba_project.handler.ImageWebSocketHandler;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
-        registry.setApplicationDestinationPrefixes("/app");
+    private final ImageWebSocketHandler imageWebSocketHandler;
+
+    public WebSocketConfig(ImageWebSocketHandler imageWebSocketHandler) {
+        this.imageWebSocketHandler = imageWebSocketHandler;
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/image-websocket")
-                .setAllowedOrigins("*")
-                .withSockJS();
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(imageWebSocketHandler, "/image-websocket") // ws://주소:포트/image-websocket으로 요청이 들어오면 websocket 통신을 진행
+                .setAllowedOrigins("*"); // 모든 ip에서 접속 가능하도록 해줌 => 이후에 특정 도메인만 허용하는 방식으로 변경할 것 (ex, "http://example.com", "https://example.com")
     }
 }
